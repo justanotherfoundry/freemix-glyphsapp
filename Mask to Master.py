8297a6c5102a3589d3692bb756c32d16ab871595
Tim Ahrens <tim@justanotherfoundry.com>
2015-11-03 20:56:01 +0100

    support selection and retain anchors

--- Mask to Master.py
+++ Mask to Master.py
@@ -14,9 +14,52 @@ in the Mac OS System Preferences.
 '''
 
 from GlyphsApp import *
+import sys
 
-font = Glyphs.font
-layers = font.selectedLayers
+def counterparts( selection, background ):
+	len_selection = len( selection )
+	best_point_range = []
+	best_deviation = sys.maxint
+	# search in each path
+	for bg_path in background.paths:
+		bg_nodes = bg_path.nodes
+		# try each starting point
+		for start in range( len( bg_nodes ) ):
+			indx = start
+			deviation = 0
+			point_range = []
+			# calculate deviation
+			for node in selection:
+				bg_node = bg_nodes[indx]
+				if node.type != bg_node.type:
+					break
+				deviation += abs( node.x - bg_node.x )
+				deviation += abs( node.y - bg_node.y )
+				if deviation > best_deviation:
+					break
+				point_range.append( bg_node )
+				# increase index looping around
+				indx = ( indx + 1 ) % len( bg_nodes )
+			else:
+				best_deviation = deviation
+				best_point_range = point_range
+	return zip( selection, best_point_range )
 
-for layer in layers:
-	layer.background.contentToBackgroundCheckSelection_keepOldBackground_(False, False)
+layer = Glyphs.font.selectedLayers[0]
+glyph = layer.parent
+selection = [ node for path in layer.paths for node in path.nodes if node in layer.selection ]
+
+glyph.beginUndo()
+
+if selection:
+	for node, bg_node in counterparts( selection, layer.background ):
+		node.position = bg_node.position
+else:
+	# delete all paths
+	while layer.paths:
+		del(layer.paths[0])
+	# insert background
+	for path in layer.copyDecomposedLayer().background.paths:
+		# copy across path
+		layer.paths.append( path )
+glyph.endUndo()
