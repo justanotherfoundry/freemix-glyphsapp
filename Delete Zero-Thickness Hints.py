cef39139f71e52962b81bf535196d149b4f14101
Tim Ahrens <tim@justanotherfoundry.com>
2015-11-06 08:57:59 +0100

    initial commit

---
+++ Delete Zero-Thickness Hints.py
@@ -0,0 +1,35 @@
+#MenuTitle: Delete Zero-Thickness Hints
+# encoding: utf-8
+
+# by Tim Ahrens
+# http://justanotherfoundry.com
+# https://github.com/justanotherfoundry/glyphsapp-scripts
+
+'''
+Removes all zero-thickness hints from all glyphs in the font.
+
+'''
+
+from GlyphsApp import *
+
+doc = Glyphs.currentDocument
+font = doc.font
+
+deletions_count = 0
+for glyph in font.glyphs:
+	for layer in glyph.layers:
+		for indx in range( len( layer.hints ) - 1, -1, -1 ):
+			hint = layer.hints[indx]
+			if hint.targetNode:
+				if hint.horizontal:
+					if hint.originNode.y == hint.targetNode.y:
+						del( layer.hints[indx] )
+						deletions_count += 1
+						print 'deleted zero-width hint from', glyph.name
+				else:
+					if hint.originNode.x == hint.targetNode.x:
+						del( layer.hints[indx] )
+						deletions_count += 1
+						print 'deleted zero-width hint from', glyph.name
+
+Message( 'Delete Zero-Thickness Hints', 'Deleted %i hints.' % deletions_count )
