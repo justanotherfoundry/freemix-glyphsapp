6daf0c3d8c995e6c548a75edcfeff3875f8a4b20
Tim Ahrens <tim@justanotherfoundry.com>
2016-07-22 10:03:24 +0200

    selects glyphs that can usually be excluded from the final exported OTF

---
+++ Select Inaccessible Glyphs.py
@@ -0,0 +1,39 @@
+#MenuTitle: Select Inaccessible Glyphs
+# encoding: utf-8
+
+'''
+Run this macro while in the Font View.
+
+The macro selects all glyphs that
+- export
+- do not have a Unicode value and 
+- are not covered by any OT feature
+
+i.e. are not accessible in the final font.
+
+These glyphs can usually be excluded from the final exported OTF font.
+
+'''
+
+from GlyphsApp import *
+import re
+
+doc = Glyphs.currentDocument
+font = doc.font
+
+# find all glyphs that are substitutions in the OT features, i.e. after the "sub ... by"
+
+# collect all features and featurePrefixes
+all_features = [ f.code for f in font.features ]
+all_features.extend( [ f.code for f in font.featurePrefixes ] )
+
+# put all into one string, without linebreaks, without brackets
+features_text = ' '.join( ' '.join( all_features ).splitlines() ).replace( ']', ' ' ).replace( '[', ' ' )
+# find substitutions via regex
+substitutions = ' '.join( re.findall( r" by ([^;]*);", features_text ) ).split()
+
+glyphs_inaccessible = [ glyph for glyph in font.glyphs if glyph.export and not glyph.name in substitutions and not glyph.name.startswith('.') and ( not glyph.glyphInfo or not glyph.glyphInfo.unicode ) ]
+
+# select inaccessible glyphs (and deselect all others)
+for glyph in font.glyphs:
+	glyph.selected = glyph in glyphs_inaccessible
