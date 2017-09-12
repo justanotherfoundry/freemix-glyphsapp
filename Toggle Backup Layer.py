8d427e3169b8702266016e2b46ea5eadaa6d5827
Tim Ahrens <tim@justanotherfoundry.com>
2017-09-12 10:33:12 +0200

    remove debug output

--- Toggle Backup Layer.py
+++ Toggle Backup Layer.py
@@ -20,18 +20,15 @@ font = Glyphs.font
 currentTab = font.currentTab
 
 layers = currentTab.layers.values()
-print layers
 currentLayer = layers[currentTab.layersCursor]
 if currentLayer.layerId == font.selectedFontMaster.id:
 	# Current layer is the selected master layer.
-	print 'Current layer is the selected master layer.'
 	# Switch current glyph to the last associated non-master layer.
 	for layer in currentLayer.parent.layers:
 		if layer.associatedMasterId == font.selectedFontMaster.id:
 			layers[currentTab.layersCursor] = layer
 else:
 	# Current layer is not the selected master layer.
-	print 'Current layer is NOT the selected master layer.'
 	# Switch each glyph in the current tab to its currently active master layer.
 	for layer_index in range( len( layers ) ):
 		layer = layers[layer_index]
