dd1d477525115f92487a06f84552ac94c888c454
Tim Ahrens <tim@justanotherfoundry.com>
2017-12-16 20:14:44 +0100

    finally works properly
    after use, the shortcuts Cmd+1, Cmd+2 etc still work

--- Toggle Backup Layer.py
+++ Toggle Backup Layer.py
@@ -18,25 +18,30 @@ from GlyphsApp import *
 
 font = Glyphs.font
 currentTab = font.currentTab
-
 layers = currentTab.layers.values()
-currentLayer = layers[currentTab.layersCursor]
-if currentLayer.layerId == font.selectedFontMaster.id:
-	# Current layer is the selected master layer.
-	# Switch current glyph to the last associated non-master layer.
-	for layer in currentLayer.parent.layers:
-		if layer.associatedMasterId == font.selectedFontMaster.id:
-			layers[currentTab.layersCursor] = layer
-else:
-	# Current layer is not the selected master layer.
-	# Switch each glyph in the current tab to its currently active master layer.
-	for layer_index in range( len( layers ) ):
-		layer = layers[layer_index]
-		glyph = layer.parent
-		master_layer = glyph.layers[ font.selectedFontMaster.id ]
-		layers[layer_index] = master_layer
-currentTab.layers.setter( layers )
 
-# explicit redraw (does not seem to be necessary)
-# currentTab.redraw()
-# Glyphs.redraw()
+string = NSMutableAttributedString.alloc().init()
+for i in xrange( len( layers ) ):
+	layer = layers[i]
+	try:
+		char = font.characterForGlyph_( layer.parent )
+	except:
+		continue
+	# initialise single char without attributes
+	# which switches the glyph to the active master
+	singleChar = NSAttributedString.alloc().initWithString_attributes_( unichr(char), {} )
+	if i == currentTab.layersCursor:
+		# we are at the currently active glyph
+		if layer.layerId == font.selectedFontMaster.id:
+			# current layer is the selected master layer.
+			# switch current glyph to the last associated non-master layer.
+			for glyphLayer in layer.parent.layers:
+				if glyphLayer.associatedMasterId == font.selectedFontMaster.id:
+					# this may happen multiple times
+					singleChar = NSAttributedString.alloc().initWithString_attributes_( unichr(char), { "GSLayerIdAttrib" : glyphLayer.layerId } )
+	else:
+		if layer.layerId != layer.associatedMasterId:
+			# user-selected layer
+			singleChar = NSAttributedString.alloc().initWithString_attributes_( unichr(char), { "GSLayerIdAttrib" : layer.layerId } )
+	string.appendAttributedString_( singleChar )
+currentTab.layers._owner.graphicView().textStorage().setText_(string)
