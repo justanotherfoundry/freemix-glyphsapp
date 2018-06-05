fd5077e9c460decc46ce46874050eae113b06fef
Tim Ahrens <tim@justanotherfoundry.com>
2018-06-05 13:54:46 +0200

    skip line breaks (i.e. jump to the next real glyph)

--- Edit Previous Glyph.py
+++ Edit Previous Glyph.py
@@ -9,4 +9,15 @@ __doc__="""
 Activates the previous glyph in the tab for editing. You can give it a keyboard shortcut in the macOS system preferences.
 """
 
-Font.currentTab.textCursor = (Font.currentTab.textCursor - 1) % len(Font.currentTab.layers)
+initialCursor = Font.currentTab.textCursor
+while 1:
+	Font.currentTab.textCursor = (Font.currentTab.textCursor - 1) % len(Font.currentTab.layers)
+	# in case there are no real glyphs in the tab, we need to prevent an infinite loop
+	if Font.currentTab.textCursor == initialCursor:
+		break
+	try:
+		if Glyphs.font.selectedLayers[0].parent.name:
+			break
+	except:
+		# this happens when the cursor reaches a line break
+		pass
