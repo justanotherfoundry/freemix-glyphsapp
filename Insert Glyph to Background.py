711fa97bb30ad0ea3916fdb6c7540e2f79b33f79
Tim Ahrens <tim@justanotherfoundry.com>
2019-10-02 09:02:38 +0200

    make inserted component selected

--- Insert Glyph to Background.py
+++ Insert Glyph to Background.py
@@ -35,6 +35,8 @@ def insert_paths( to_layer, from_layer, alignment, as_component_is_checked, clea
 		shift = ( to_layer.width - from_layer.width ) if alignment == RIGHT else 0
 		from_glyph_name = from_layer.parent.name
 		to_layer.background.components.append( GSComponent( from_glyph_name, NSPoint( shift, 0 ) ) )
+		# select component (makes is quicker to move around the shape later)
+		to_layer.background.components[-1].selected = True
 	else:
 		# insert all paths
 		for path in from_layer.copyDecomposedLayer().paths:
