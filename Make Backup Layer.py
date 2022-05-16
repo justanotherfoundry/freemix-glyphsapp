b102b75ff35b7e4d144646b71a3598bb6ea18e14
Just Another Foundry <tim@justanotherfoundry.com>
2022-05-16 14:39:00 +0200

    Fix copying brace layers

--- Make Backup Layer.py
+++ Make Backup Layer.py
@@ -6,15 +6,22 @@
 # https://github.com/justanotherfoundry/glyphsapp-scripts
 
 __doc__="""
-Same as the “Copy” button on the Layers palette but as a script. Because I really want a keyboard shortcut for this.
+Same as the “+” button on the Layers palette.
 
-Without a keyboard shortcut this script is completely useless. Sorry.
+I wrote this script mostly because it allows me to assign a keyboard shortcut. Also, it correctly copies brace layers, unlike Glyphs’ built-in function.
 """
+
+GSLayer.isBraceLayer = property(lambda self: bool(self.pyobjc_instanceMethods.isBraceLayer()),
+								lambda self, value: self.setBraceLayer_(value) )
 
 import datetime
 
 for layer in Glyphs.font.selectedLayers:
 	glyph = layer.parent
 	newLayer = layer.copy()
-	newLayer.name = datetime.datetime.now().strftime( "%b %-d %y, %H:%M" )
+	if newLayer.isBraceLayer:
+		newLayer.name = newLayer.name + datetime.datetime.now().strftime( " %b %-d %y, %H:%M" )
+		newLayer.isBraceLayer = False
+	else:
+		newLayer.name = datetime.datetime.now().strftime( "%b %-d %y, %H:%M" )
 	glyph.layers.append( newLayer )
