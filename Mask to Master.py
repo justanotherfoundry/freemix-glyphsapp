ea854a36084c53610911c71984001976e4f4feb2
Just Another Foundry <tim@justanotherfoundry.com>
2023-06-26 12:12:37 +0200

    if no changes, beep instead of setting up undo

--- Mask to Master.py
+++ Mask to Master.py
@@ -6,12 +6,13 @@
 # https://github.com/justanotherfoundry/glyphsapp-scripts
 
 __doc__='''
-The selected nodes will adopt the position of the corresponding nodes in the mask.
+The selected nodes will adopt the position of the corresponding nodes in the background.
 In combination with Insert Glyph to Background, you can easily
 transfer parts of the outlines between glyphs.
 '''
 
 import sys
+from AppKit import NSBeep
 
 def counterparts( selection, background ):
 	if background.components:
@@ -89,22 +90,47 @@ layer = Glyphs.font.selectedLayers[0]
 glyph = layer.parent
 selection = [ node for path in layer.paths for node in path.nodes if node in layer.selection ]
 
-glyph.beginUndo()
-layer.beginChanges()
+any_changes = False
 if selection:
 	subpaths = subpaths( selection )
 	for subpath in subpaths:
 		for node, bg_node in counterparts( subpath, layer.background ):
+			if not any_changes:
+				if node.position == bg_node.position:
+					continue
+				else:
+					any_changes = True
+					glyph.beginUndo()
+					layer.beginChanges()
 			node.position = bg_node.position
 else:
-	try:
-		while layer.shapes:
-			del(layer.shapes[0])
-	except:
-		while layer.paths:
-			del(layer.paths[0])
-	for path in layer.background.copyDecomposedLayer().paths:
-		layer.paths.append( path.copy() )
-layer.syncMetrics()
-layer.endChanges()
-glyph.endUndo()
+	decomposed_bg = layer.background.copyDecomposedLayer()
+	if len( layer.paths ) != len( decomposed_bg.paths ):
+		any_changes = True
+	else:
+		for path, bg_path in zip(layer.paths, decomposed_bg.paths):
+			if len( path.nodes ) != len( bg_path.nodes ):
+				any_changes = True
+				break
+			else:
+				for node, bg_node in zip(path.nodes, bg_path.nodes):
+					if node.position != bg_node.position:
+						any_changes = True
+						break
+	if any_changes:
+		glyph.beginUndo()
+		layer.beginChanges()
+		try:
+			while layer.shapes:
+				del(layer.shapes[0])
+		except:
+			while layer.paths:
+				del(layer.paths[0])
+		for path in decomposed_bg.paths:
+			layer.paths.append( path.copy() )
+if any_changes:
+	layer.syncMetrics()
+	layer.endChanges()
+	glyph.endUndo()
+else:
+	NSBeep()
