42ea3722a06190609a88ef79a091666eb9802f3a
Just Another Foundry <tim@justanotherfoundry.com>
2023-09-25 14:06:14 +0200

    remove debug output

--- Remove Backup Layers.py
+++ Remove Backup Layers.py
@@ -20,5 +20,4 @@ selected_glyphs = set( [ layer.parent for layer in font.selectedLayers ] )
 for glyph in selected_glyphs:
 	associated_layers = [ layer.layerId for layer in glyph.layers if layer.layerId != layer.associatedMasterId and ( not layer.name or not re.search( r"(\{[^a-zA-Z]+\})|([\[\]][^a-zA-Z]+\])", layer.name ) ) ]
 	for layerId in associated_layers:
-		print( 'deleting extra layer from', glyph.name )
 		del glyph.layers[layerId]
