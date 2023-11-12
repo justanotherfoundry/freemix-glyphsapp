dd93b6566c062933712578c39a37d64f1baf4c9c
Just Another Foundry <tim@justanotherfoundry.com>
2023-11-12 09:40:36 +0100

    tidy up

--- Edit Next Glyph.py
+++ Edit Next Glyph.py
@@ -10,21 +10,20 @@ Activates the next glyph in the tab for editing. You can give it a keyboard shor
 """
 
 font = Glyphs.font
-initialCursor = font.currentTab.textCursor
+tab = font.currentTab
+initialCursor = tab.textCursor
 while 1:
-	font.currentTab.textCursor = (font.currentTab.textCursor + 1) % len(font.currentTab.layers)
+	tab.textCursor = (tab.textCursor + 1) % len(tab.layers)
 	# in case there are no real glyphs in the tab, we need to prevent an infinite loop
-	if font.currentTab.textCursor == initialCursor:
+	if tab.textCursor == initialCursor:
 		break
 	try:
-		if Glyphs.font.selectedLayers[0].parent.name:
+		if font.selectedLayers[0].parent.name:
 			break
 	except:
 		# this happens when the cursor reaches a line break
 		pass
-
-layer = Glyphs.font.selectedLayers[0]
-tab = Glyphs.font.currentTab
+layer = font.selectedLayers[0]
 if tab.viewPort.origin.x + tab.viewPort.size.width < tab.bounds.origin.x + tab.bounds.size.width:
 	vp = tab.viewPort
 	vp.origin.x = tab.selectedLayerOrigin.x + 0.5 * ( layer.width * tab.scale - vp.size.width )
