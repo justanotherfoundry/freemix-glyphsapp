c99863015061abfae1272e55613ae27d3299f8ae
Just Another Foundry <tim@justanotherfoundry.com>
2024-11-14 10:27:52 +0100

    treat all layers

--- Delete BCP.py
+++ Delete BCP.py
@@ -13,20 +13,21 @@ If only one BCP in the curve remains then this makes it a quadratic Bézier.
 for selectedLayer in Glyphs.currentDocument.selectedLayers():
 	glyph = selectedLayer.parent
 	glyph.beginUndo()
-	for path in layer.paths:
-		for i in range(len(path.nodes) - 1, -1, -1):
-			node = path.nodes[i]
-			if node.selected and node.type == OFFCURVE:
-				if node.nextNode.type == OFFCURVE:
-					# node is the first cubic BCP
-					node.nextNode.nextNode.type = QCURVE
-				elif node.prevNode.type == OFFCURVE:
-					# node is the second cubic BCP
-					node.nextNode.type = QCURVE
-				else:
-					# curve is quadratic and is becoming a straight line
-					node.nextNode.type = LINE
-					node.nextNode.smooth = False
-					node.prevNode.smooth = False
-				del path.nodes[i]
+	for layer in glyph.layers:
+		for path in layer.paths:
+			for i in range(len(path.nodes) - 1, -1, -1):
+				node = path.nodes[i]
+				if node.selected and node.type == OFFCURVE:
+					if node.nextNode.type == OFFCURVE:
+						# node is the first cubic BCP
+						node.nextNode.nextNode.type = QCURVE
+					elif node.prevNode.type == OFFCURVE:
+						# node is the second cubic BCP
+						node.nextNode.type = QCURVE
+					else:
+						# curve is quadratic and is becoming a straight line
+						node.nextNode.type = LINE
+						node.nextNode.smooth = False
+						node.prevNode.smooth = False
+					del path.nodes[i]
 	glyph.endUndo()
