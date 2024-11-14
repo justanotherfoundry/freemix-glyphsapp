728f7b9a12235739eb475e889950672bffb93a43
Just Another Foundry <tim@justanotherfoundry.com>
2024-11-14 10:29:12 +0100

    when deleting retracted BCPs, adjust the other handle length

--- Delete BCP.py
+++ Delete BCP.py
@@ -21,9 +21,24 @@ for selectedLayer in Glyphs.currentDocument.selectedLayers():
 					if node.nextNode.type == OFFCURVE:
 						# node is the first cubic BCP
 						node.nextNode.nextNode.type = QCURVE
+						if node.position == node.prevNode.position:
+							# to-be-deleted BCP is retracted
+							bcp = node.nextNode.position
+							h2_x = node.nextNode.nextNode.position.x - bcp.x
+							h2_y = node.nextNode.nextNode.position.y - bcp.y
+							bcp.x = round( node.nextNode.nextNode.position.x - h2_x * 0.9 )
+							bcp.y = round( node.nextNode.nextNode.position.y - h2_y * 0.9 )
+							node.nextNode.position = bcp
 					elif node.prevNode.type == OFFCURVE:
 						# node is the second cubic BCP
 						node.nextNode.type = QCURVE
+						if node.position == node.nextNode.position:
+							bcp = node.prevNode.position
+							h1_x = bcp.x - node.prevNode.prevNode.position.x
+							h1_y = bcp.y - node.prevNode.prevNode.position.y
+							bcp.x = round( node.prevNode.prevNode.position.x + h1_x * 0.9 )
+							bcp.y = round( node.prevNode.prevNode.position.y + h1_y * 0.9 )
+							node.prevNode.position = bcp
 					else:
 						# curve is quadratic and is becoming a straight line
 						node.nextNode.type = LINE
