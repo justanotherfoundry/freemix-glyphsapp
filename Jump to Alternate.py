a6b7483f1d7980e4c71b2ee11788a313cb3b72cd
Just Another Foundry <tim@justanotherfoundry.com>
2024-12-27 11:52:25 +0100

    If several glyphs are selected, add or remove suffixes

--- Jump to Alternate.py
+++ Jump to Alternate.py
@@ -10,9 +10,12 @@ In the edit view, use this script to “jump” back and forth (or to circle)
 between alternate glyphs such as one, one.lf and one.tosf.
 
 Tip: Give it a keyboard shortcut!
+
+If several glyphs are selected you can choose to add or remove suffixes.
 """
 
 from builtins import chr
+import vanilla
 font = Glyphs.font
 
 def replaceInDisplayString(newString):
@@ -20,7 +23,6 @@ def replaceInDisplayString(newString):
 	textStorage = graphicView.textStorage()
 	text = textStorage.text()
 	selectedRange = graphicView.selectedRange()
-	selectedRange.length = 1
 	while 1:
 		selectedRange.length += 1
 		try:
@@ -37,7 +39,49 @@ def replaceInDisplayString(newString):
 	text.replaceCharactersInRange_withString_(selectedRange, newString)
 	textStorage.didChangeValueForKey_('text')
 
+class JumpDialog(object):
+
+	def __init__(self):
+		windowWidth = 300
+		windowHeight = 125
+		try:
+			self.layer = doc.selectedLayers()[0]
+		except TypeError:
+			self.layer = None
+			return
+		self.w = vanilla.FloatingWindow((windowWidth, windowHeight), 'Jump to Alternates', autosaveName='com.freemix.jump_to_alternate.mainwindow')
+		leftMargin = 20
+		rightMargin = -20
+		elementHeight = 20
+		posY = 12
+		self.w.removeSuffixLabel = vanilla.TextBox((leftMargin, posY + 1, 115, elementHeight), "Remove suffix")
+		self.w.removeSuffix = vanilla.EditText((120, posY, rightMargin, elementHeight))
+		posY += elementHeight + 10
+		self.w.addSuffixLabel = vanilla.TextBox((leftMargin, posY + 1, 115, elementHeight), "Add suffix")
+		self.w.addSuffix = vanilla.EditText((120, posY, rightMargin, elementHeight))
+		self.w.jumpButton = vanilla.Button((leftMargin, -40, -10, rightMargin), 'Replace with alternates', callback=self.buttonCallback)
+		self.w.setDefaultButton(self.w.jumpButton)
+
+	def buttonCallback(self, sender):
+		newText = ''
+		for layer in font.selectedLayers:
+			glyph = layer.parent
+			glyphName = glyph.name.replace(self.w.removeSuffix.get(), '')
+			glyphName += self.w.addSuffix.get()
+			nextGlyph = font.glyphs[glyphName]
+			if nextGlyph:
+				glyph = nextGlyph
+			nextChar = chr(font.characterForGlyph(glyph))
+			newText += nextChar
+		replaceInDisplayString(newText)
+		self.w.close()
+
 def jumpToAlternate():
+	if len(font.selectedLayers) > 1:
+		dialog = JumpDialog()
+		dialog.w.open()
+		dialog.w.makeKey()
+		return
 	# find new glyph:
 	try:
 		currentLayer = font.selectedLayers[0]
