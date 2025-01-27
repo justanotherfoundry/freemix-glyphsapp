508fb5abbd7fb62c0990f4a3429cb36c3dc4c2b8
Just Another Foundry <tim@justanotherfoundry.com>
2025-01-27 14:47:58 +0100

    new class class FMXpoint

--- Symmetrify Terminal.py
+++ Symmetrify Terminal.py
@@ -11,76 +11,110 @@ __doc__='''
 
 import math
 
+class FMXpoint:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	@classmethod
+	def fromNode(cls, glyphsNode):
+		return cls(glyphsNode.position.x, glyphsNode.position.y)
+
+	def assignToNode(self, glyphsNode):
+		pos = glyphsNode.position
+		pos.x = self.x
+		pos.y = self.y
+		glyphsNode.position = pos
+		# note: Glyphs will do the rounding as applicable
+
+	def __repr__(self):
+		return f"FMXpoint(x={self.x}, y={self.y})"
+
+	def __add__(self, other):
+		return FMXpoint(self.x + other.x, self.y + other.y)
+
+	def __iadd__(self, other):
+		self.x += other.x
+		self.y += other.y
+		return self
+
+	def __sub__(self, other):
+		return FMXpoint(self.x - other.x, self.y - other.y)
+
+	def __mul__(self, scalar):
+		return FMXpoint(self.x * scalar, self.y * scalar)
+
+	def lengthSquared(self):
+		return self.x ** 2 + self.y ** 2
+
+	def normal(self):
+		return FMXpoint(self.y, self.x)
+
+	def length(self):
+		return math.sqrt(self.lengthSquared())
+
+	def atLength(self, newLength):
+		if isinstance(newLength, FMXpoint):
+			return self * math.sqrt(newLength.lengthSquared() / self.lengthSquared())
+		else:
+			return self * (newLength / self.length())
+
+	def isShorterThan(self, other):
+		return self.lengthSquared() < other.lengthSquared()
+	
+	def rounded(self):
+		return FMXpoint(round(self.x), round(self.y))
+
+	# returns the rounding error
+	def roundInPlace(self):
+		unrounded = self
+		self.x = round(self.x)
+		self.y = round(self.y)
+		return self - unrounded
+
+	# returns the dor product
+	def dot(self, other):
+		return self.x * other.x + self.y * other.y
+
+	# projects the vector onto another
+	# • the returned vector is parallel to other
+	# • self and the returned vector form a rectangular triangle
+	def alignedTo(self, other):
+		if other.x == 0:
+			return (0, y)
+		elif other.y == 0:
+			return (x, 0)
+		else:
+			return other * (self.dot(other) / other.lengthSquared())
+
+	@classmethod
+	#  this seems more sensible as a class method
+	#  so as to reflect the conceptual symmetry
+	def dist(cls, p1, p2):
+		return (p1 - p2).length()
+
 def samePosition(node1, node2):
 	return node1.position.x == node2.position.x and node1.position.y == node2.position.y
 
 def intersection(p1, p2, p3, p4):
-	x1, y1 = p1
-	x2, y2 = p2
-	x3, y3 = p3
-	x4, y4 = p4
-	A1 = y2 - y1
-	B1 = x1 - x2
-	C1 = A1 * x1 + B1 * y1
-	A2 = y4 - y3
-	B2 = x3 - x4
-	C2 = A2 * x3 + B2 * y3
+	A1 = p2.y - p1.y
+	B1 = p1.x - p2.x
+	C1 = A1 * p1.x + B1 * p1.y
+	A2 = p4.y - p3.y
+	B2 = p3.x - p4.x
+	C2 = A2 * p3.x + B2 * p3.y
 	det = A1 * B2 - A2 * B1
 	if det == 0:
 		# lines are parallel
 		return None
 	x = (B2 * C1 - B1 * C2) / det
 	y = (A1 * C2 - A2 * C1) / det
-	return (x, y)
+	return FMXpoint(x, y)
 
-def vPlus(v1, v2):
-	x1, y1 = v1
-	x2, y2 = v2
-	return (x1 + x2, y1 + y2)
-
-def vMinus(v1, v2):
-	x1, y1 = v1
-	x2, y2 = v2
-	return (x1 - x2, y1 - y2)
-
-def vMult(v, s):
-	x, y = v
-	return (x * s, y * s)
-
-def vectorLengthSquared(v):
-	x, y = v
-	return x * x + y * y
-
-def vectorLength(v):
-	x, y = v
-	return math.sqrt(x * x + y * y)
-
-def pointDist(p1, p2):
-	return vectorLength(vMinus(p1, p2))
-
-def dotProduct(v1, v2):
-	x1, y1 = v1
-	x2, y2 = v2
-	return x1 * x2 + y1 * y2
-
-def aligned(v1, v2):
-	x1, y1 = v1
-	x2, y2 = v2
-	if x2 == 0:
-		return (0, y1)
-	elif y2 == 0:
-		return (x1, 0)
-	else:
-		return vMult(v2, dotProduct(v1, v2) / vectorLengthSquared(v2))
-
-def rounded(v):
-	x, y = v
-	return (round(x), round(y))
-
-def setNodePosition(node, p):
-	pos = node.position
-	pos.x, pos.y = p
-	node.position = pos
+# if the vertex is further away that this
+# the sides of the stroke will be considered parallel:
+# (this is mostly because of float imprecision)
+MAX_VERTEX_DIST = 3000
 
 for selectedLayer in Font.selectedLayers:
 	glyph = selectedLayer.parent
@@ -91,74 +125,64 @@ for selectedLayer in Font.selectedLayers:
 				n1 = node
 				n2 = n1.nextNode
 				n3 = n2.nextNode
+				if samePosition(n1, n2):
+					# pointed terminal, perpendicularity not applicable
+					continue
 				if samePosition(n0, n1):
 					# retracted BCP
 					n0 = n0.prevNode
 				if samePosition(n2, n3):
 					# retracted BCP
 					n3 = n3.nextNode
-				p0 = (n0.position.x, n0.position.y)
-				p1 = (n1.position.x, n1.position.y)
-				p2 = (n2.position.x, n2.position.y)
-				p3 = (n3.position.x, n3.position.y)
+				p0 = FMXpoint.fromNode(n0)
+				p1 = FMXpoint.fromNode(n1)
+				p2 = FMXpoint.fromNode(n2)
+				p3 = FMXpoint.fromNode(n3)
 				vertex = intersection(p0, p1, p2, p3)
-				if vertex and pointDist(p1, vertex) > 32000:
+				if vertex and FMXpoint.dist(p1, vertex) > MAX_VERTEX_DIST:
 					vertex = None
 				if vertex:
-					v1 = vMinus(p1, vertex)
-					v2 = vMinus(p2, vertex)
-					d1 = vectorLength(v1)
-					d2 = vectorLength(v2)
-					d_new = (d1 + d2) / 2
-					f1 = d_new / d1
-					f2 = d_new / d2
-					p1_new = vPlus(vertex, vMult(v1, f1))
-					p2_new = vPlus(vertex, vMult(v2, f2))
-				else:
-					v1 = vMinus(p1, p0)
-					v12 = vMinus(p2, p1)
-					v1_shift = vMult(aligned(v12, v1), 0.5)
-					p1_new = vPlus(p1, v1_shift)
-					p2_new = vMinus(p2, v1_shift)
-				if Font.grid != 0:
-					p1_rounded = rounded(p1_new)
-					p2_rounded = rounded(p2_new)
-					p1_roundingError = vMinus(p1_rounded, p1_new)
-					p2_roundingError = vMinus(p2_rounded, p2_new)
-					if vectorLengthSquared(p1_roundingError) < vectorLengthSquared(p2_roundingError):
-						# use p1_rounded (as it has a smaller rounding error):
-						setNodePosition(n1, p1_rounded)
-						if vertex:
-							# tweak v2 so as to adopt the rounding error:
-							v2 = vPlus(v2, p1_roundingError)
-							# update values:
-							v1 = vMinus(p1_rounded, vertex)
-							d1 = vectorLength(v1)
-							d2 = vectorLength(v2)
-							# set node 2 to match d1:
-							f2 = d1 / d2
-							p2_new = vPlus(vertex, vMult(v2, f2))
+					v1 = p1 - vertex
+					v2 = p2 - vertex
+					lenNew = (v1.length() + v2.length()) / 2
+					p1new = vertex + v1.atLength(lenNew)
+					p2new = vertex + v2.atLength(lenNew)
+					if Font.grid != 0:
+						p1roundingError = p1new.roundInPlace()
+						p2roundingError = p2new.roundInPlace()
+						if p1roundingError.isShorterThan(p2roundingError):
+							# seems better to keep p1new and update p2new.
+							# let’s tweak p2 so as to adopt the rounding error:
+							# (we prefer to shift the stroke rather than changing its weight)
+							p2 += p1roundingError
+							# updating the vertex may be overly perfectionist
+							# but let’s do all we can to end up with perpendicularity:
+							vertex = intersection(p0, p1new, p2, p3)
+							v1 = p1new - vertex
+							v2 = p2 - vertex
+							# we prioritise perpendicularity over stroke length
+							# so let’s adopt the given v1 length:
+							# (this means p1p2 does not generally rotate around its middle)
+							p2new = vertex + v2.atLength(v1)
 						else:
-							p2 = vPlus(p2, p1_roundingError)
-							v21 = vMinus(p1_rounded, p2)
-							v2_shift = aligned(v21, v1)
-							p2_new = vPlus(p2, v2_shift)
-						setNodePosition(n2, p2_new) # Glyphs will do the rounding
-					else:
-						setNodePosition(n2, p2_rounded)
-						if vertex:
-							v1 = vPlus(v1, p2_roundingError)
-							v2 = vMinus(p2_rounded, vertex)
-							d1 = vectorLength(v1)
-							d2 = vectorLength(v2)
-							f1 = d2 / d1
-							p1_new = vPlus(vertex, vMult(v1, f1))
-						else:
-							p1 = vPlus(p1, p2_roundingError)
-							v12 = vMinus(p2_rounded, p1)
-							v1_shift = aligned(v12, v1)
-							p1_new = vPlus(p1, v1_shift)
-						setNodePosition(n1, p1_new) # Glyphs will do the rounding
+							# same process as above:
+							p1 += p2roundingError
+							vertex = intersection(p0, p1, p2new, p3)
+							v1 = p1 - vertex
+							v2 = p2new - vertex
+							p1new = vertex + v1.atLength(v2)
 				else:
-					setNodePosition(n1, p1_new)
-					setNodePosition(n2, p2_new)
+					# parallel sides
+					shift = (p2 - p1).alignedTo(p1 - p0) * 0.5
+					p1new = p1 + shift
+					if Font.grid != 0:
+						# note: because of symmetry, the rounding error 
+						#       would be the same for p1 and p2
+						p1roundingError = p1new.roundInPlace()
+						# similar to the above, adopt the rounding error:
+						p2 += p1roundingError
+						# update the shift for p2new:
+						shift = (p2 - p1new).alignedTo(p1new - p0)
+					p2new = p2 - shift
+				p1new.assignToNode(n1)
+				p2new.assignToNode(n2)
