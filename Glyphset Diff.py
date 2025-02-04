61e2de3ccba4e28a523f882eac708dcf47ef1758
Just Another Foundry <tim@justanotherfoundry.com>
2025-02-04 11:55:11 +0100

    refactor with function

--- Glyphset Diff.py
+++ Glyphset Diff.py
@@ -12,16 +12,20 @@ Shows the glyphs that are not present in the other font.
 from GlyphsApp import Glyphs, Message
 from Cocoa import NSPredicate
 
+def setFilter(font, glyphNames):
+	if not glyphNames:
+		return
+	fontView = font.fontView
+	glyphsArrayController = fontView.glyphsArrayController()
+	predicate = NSPredicate.predicateWithFormat_("name IN %@", glyphNames)
+	glyphsArrayController.setFilterPredicate_(predicate)
+
 if len(Glyphs.documents) == 2:
 	thisFont = Glyphs.documents[0].font
 	otherFont = Glyphs.documents[1].font
 
 	otherGlyphnames = set(otherFont.glyphNames())
 	diff = [glyph.name for glyph in thisFont.glyphs if glyph.name not in otherGlyphnames]
-	fontView = thisFont.fontView
-	glyphsArrayController = fontView.glyphsArrayController()
-	
-	predicate = NSPredicate.predicateWithFormat_("name IN %@", diff)
-	glyphsArrayController.setFilterPredicate_(predicate)
+	setFilter(thisFont, diff)
 else:
 	Message('Please make sure that\nexactly two fonts are open', '')
