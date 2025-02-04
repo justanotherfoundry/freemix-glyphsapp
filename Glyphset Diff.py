b0273dae942a3e6b9506dca6e73feef07b9141cd
Just Another Foundry <tim@justanotherfoundry.com>
2025-02-04 11:49:50 +0100

    fix error
    as per https://forum.glyphsapp.com/t/create-list-filter-via-script/2134/25

--- Glyphset Diff.py
+++ Glyphset Diff.py
@@ -9,18 +9,19 @@ __doc__ = '''
 Shows the glyphs that are not present in the other font.
 '''
 
-from GlyphsApp import *
+from GlyphsApp import Glyphs, Message
+from Cocoa import NSPredicate
 
 if len(Glyphs.documents) == 2:
 	thisFont = Glyphs.documents[0].font
 	otherFont = Glyphs.documents[1].font
 
 	otherGlyphnames = set(otherFont.glyphNames())
-	diff = [glyph for glyph in thisFont.glyphs if glyph.name not in otherGlyphnames]
+	diff = [glyph.name for glyph in thisFont.glyphs if glyph.name not in otherGlyphnames]
 	fontView = thisFont.fontView
 	glyphsArrayController = fontView.glyphsArrayController()
 	
-	predicate = fontView.predicateForNameList_negative_(diff, False)
+	predicate = NSPredicate.predicateWithFormat_("name IN %@", diff)
 	glyphsArrayController.setFilterPredicate_(predicate)
 else:
 	Message('Please make sure that\nexactly two fonts are open', '')
