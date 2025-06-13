832fc6ef8ff990767d288765c6a764a49ce4310f
Just Another Foundry <tim@justanotherfoundry.com>
2025-06-13 15:34:00 +0200

    general update

--- Expand Kerning.py
+++ Expand Kerning.py
@@ -1,12 +1,11 @@
 #MenuTitle: Expand Kerning
-# encoding: utf-8
 
 # by Tim Ahrens
 # http://justanotherfoundry.com
 # https://github.com/justanotherfoundry/freemix-glyphsapp
 
 '''
-Expand Kerning like we know it from FontLab.
+Converts the class kerning to glyph-glyph pairs
 
 '''
 
@@ -20,18 +19,20 @@ glyph_names = set( [ glyph.name for glyph in font.glyphs ] )
 right_sides = right_groups.union( glyph_names )
 
 # expand
-for left_glyph in font.glyphs:
-	if left_glyph.rightKerningGroup:
-		left_name = '@MMK_L_' + left_glyph.rightKerningGroup
-	else:
-		left_name = left_glyph.name
-	for right_glyph in font.glyphs:
+flatKerning = { master_id: {} }
+kerningDict = flatKerning[master_id]
+for leftGlyph in font.glyphs:
+	leftLayer = leftGlyph.layers[master_id]
+	for rightGlyph in font.glyphs:
+		rightLayer = rightGlyph.layers[master_id]
+		kernValue = font.kerningFirstLayer_secondLayer_(leftLayer, rightLayer)
+
 		if right_glyph.leftKerningGroup:
 			right_name = '@MMK_R_' + right_glyph.leftKerningGroup
 		else:
 			right_name = right_glyph.name
 		value = font.kerningForPair( master_id, left_name, right_name )
-		if value is not None and value < 77000:
+		if value is not None and value != 0 and value < 77000:
 			existing_value = font.kerningForPair( master_id, left_glyph.name, right_glyph.name )
 			if existing_value is not None and existing_value < 77000:
 				continue
@@ -41,15 +42,19 @@ for left_glyph in font.glyphs:
 			exception_value = font.kerningForPair( master_id, left_name, right_glyph.name )
 			if exception_value is not None and exception_value < 77000:
 				value = exception_value
-			font.setKerningForPair( master_id, left_glyph.name, right_glyph.name, value )
+			try:
+				glyphKerningDict = kerningDict[left_glyph.id]
+			except KeyError:
+				kerningDict[left_glyph.id] = {}
+				glyphKerningDict = kerningDict[left_glyph.id]
+# 			glyphKerningDict[right_glyph.id] = value
+# 			font.setKerningForPair( master_id, left_glyph.name, right_glyph.name, value )
+			flatKerning.append((left_glyph.name, right_glyph.name, value))
 
-# remove group kerning
-for left_side in left_groups:
-	for right_side in right_sides:
-		font.removeKerningForPair( master_id, left_side, right_side )
-for left_side in glyph_names:
-	for right_side in right_groups:
-		font.removeKerningForPair( master_id, left_side, right_side )
+font.kerning = {}
+print(len(flatKerning), 'flat kerning pairs')
+for left_glyph_name, right_glyph_name, value in flatKerning:
+	font.setKerningForPair( master_id, left_glyph_name, right_glyph_name, value )
 
 # remove kerning groups
 # (TODO)
