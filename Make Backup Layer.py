2fc5409d9bdb169e8cd02e5d8833b8119022ffb6
Just Another Foundry <1331354+justanotherfoundry@users.noreply.github.com>
2025-11-19 10:48:18 +0100

    fix making a backup of intermediate layer

--- Make Backup Layer.py
+++ Make Backup Layer.py
@@ -10,18 +10,23 @@ Same as the “+” button on the Layers palette.
 I wrote this script mostly because it allows me to assign a keyboard shortcut. Also, it correctly copies brace layers, unlike Glyphs’ built-in function.
 '''
 
-GSLayer.isBraceLayer = property(lambda self: bool(self.pyobjc_instanceMethods.isBraceLayer()),
-								lambda self, value: self.setBraceLayer_(value) )
-
 import datetime
+import Foundation
+
+formatter = Foundation.NSDateFormatter.alloc().init()
+formatter.setDateStyle_(Foundation.NSDateFormatterMediumStyle)
+formatter.setTimeStyle_(Foundation.NSDateFormatterShortStyle)
+now = Foundation.NSDate.date()
+nowString = formatter.stringFromDate_(now).replace(' 202', ' 2')
+# ^ seems to be the best way to create the same as Glyphs, as of now
 
 for layer in Glyphs.font.selectedLayers:
 	glyph = layer.parent
 	newLayer = layer.copy()
 	if newLayer.isSpecialLayer:
-		newLayer.name = newLayer.name + datetime.datetime.now().strftime( " %b %-d %y, %H:%M" )
+		newLayer.name = '{.} ' + nowString
 		del newLayer.attributes["axisRules"]
 		del newLayer.attributes["coordinates"]
 	else:
-		newLayer.name = datetime.datetime.now().strftime( "%b %-d %y, %H:%M" )
+		newLayer.name = nowString
 	glyph.layers.append( newLayer )
